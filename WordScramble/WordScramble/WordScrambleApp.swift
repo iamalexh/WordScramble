b22bc5942c20bb9053a864d1646dcfa5e7acb55d
iamalexh <67149386+iamalexh@users.noreply.github.com>
2020-11-03 13:57:20 +0000

    Add files via upload

---
+++ WordScramble/WordScramble/WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrambleApp.swift
+//  WordScramble
+//
+//  Created by Alex on 19/08/2020.
+//
+
+import SwiftUI
+
+@main
+struct WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
